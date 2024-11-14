87afdf9991c45dae32b481d39aeee5a3443a405f
Claudio <claudiogonzalez0712@gmail.com>
2024-11-14 12:51:21 -0300

    14-11-24 correccion de vistas de Chile

--- resources/views/chile/fouls.blade.php
+++ resources/views/chile/fouls.blade.php
@@ -4,7 +4,7 @@
        
         <div id="corners" class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <livewire:fouls :pais="'Chile'" :liga="'chile-primera-division'" :temPorDefecto="'ChileStat2024'"  :temp2024="'ChileStat2024'" />
+                <livewire:fouls :pais="'Chile'" :liga="'chile-primera-division'" :nombreModelo="'ChileStat'"/>
             </div>
         </div>
     </div>
--- resources/views/chile/ht-ft.blade.php
+++ resources/views/chile/ht-ft.blade.php
@@ -4,7 +4,7 @@
        
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <livewire:half-time-full-time :pais="'Chile'" :liga="'chile-primera-division'" :temPorDefecto="'Chile_primera_division2024'" :temp2024="'Chile_primera_division2024'" :temp2023="'Chile_primera_division2023'" :temp2022="'Chile_primera_division2022'" :temp2021="'Chile_primera_division2021'" :temp2020="'Chile_primera_division2020'" />
+                <livewire:half-time-full-time :pais="'Chile'" :liga="'chile-primera-division'"  :nombreModelo="'Chile_primera_division'"/>
             </div>
         </div>
     </div>
