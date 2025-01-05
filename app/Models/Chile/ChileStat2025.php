a34a405d515a75d9f652034c42ae1636a9fcebf5
Claudio <claudiogonzalez0712@gmail.com>
2025-01-05 00:28:53 -0300

    se agrega modelo Chile

---
+++ app/Models/Chile/ChileStat2025.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Chile;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ChileStat2025 extends Model
+{
+    use HasFactory;
+    protected $table = 'chile_primera_divisionstats2025';
+    protected $guarded = [];
+}
