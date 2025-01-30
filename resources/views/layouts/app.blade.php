575c39829848309e12b39ef7e43ae1fce6362c9a
Claudio <claudiogonzalez0712@gmail.com>
2025-01-30 00:22:31 -0300

    Proyecto casi terminado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,17 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="Consulta estadísticas en vivo de LaLiga, Premier League, Champions y más. Análisis de goles, corners y rendimiento para apuestas deportivas.">
 
-    <title>{{ config('app.name', 'Futbol168') }}</title>
-    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image">
+    <meta property="og:title" content="Estadísticas de Fútbol en Vivo | LaLiga, Premier y Más">
+    <meta property="og:description" content="Consulta datos de goles, tarjetas, corners y rendimiento de equipos en vivo. Ideal para análisis de apuestas deportivas.">
+    <meta property="og:image" content="{{ asset('miniatura.png') }}">
+    <meta property="og:url" content="{{ url()->current() }}">
+   
+
+    <title>⚽ Estadísticas de Fútbol en Vivo | LaLiga, Premier, Champions y Más</title>
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('favicon.svg') }}" type="image/svg+xml">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -25,7 +33,10 @@
 </head>
 
 <body class="font-sans antialiased">
-
+    <h1 style="display: none;">Estadísticas de Fútbol para apuestas deportivas</h1>
+    <section style="display: none;">
+        <p>Las mejores estadísticas de fútbol de LaLiga, Premier League, Bundesliga, Primeira Liga, Serie A, Champions League, Copa Libertadores y más.</p>
+    </section>
     <div style="background-color: #3D8361;" class="min-h-screen">
         @livewire('navigation-menu')
 
@@ -45,10 +56,10 @@
     </div>
 
     <footer>
-    <p>&copy; <span id="currentYear"></span> Claudio González Delgado. Todos los derechos reservados.</p>
-    <p>Contacto: <a href="mailto:claudio@futbol168.net">claudio@futbol168.net</a></p>
-</footer>
-<script>
+        <p>&copy; <span id="currentYear"></span> Claudio González Delgado. Todos los derechos reservados.</p>
+        <p>Contacto: <a href="mailto:claudio@futbol168.net">claudio@futbol168.net</a></p>
+    </footer>
+    <script>
         document.getElementById('currentYear').innerText = new Date().getFullYear();
     </script>
     @stack('modals')
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,27 +1,39 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Futbo168') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="Consulta estadísticas en vivo de LaLiga, Premier League, Champions y más. Análisis de goles, corners y rendimiento para apuestas deportivas.">
+    <meta property="og:title" content="Estadísticas de Fútbol en Vivo | LaLiga, Premier y Más">
+    <meta property="og:description" content="Consulta datos de goles, tarjetas, corners y rendimiento de equipos en vivo. Ideal para análisis de apuestas deportivas.">
+    <meta property="og:image" content="{{ asset('miniatura.png') }}">
+    <meta property="og:url" content="{{ url()->current() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>⚽ Estadísticas de Fútbol en Vivo | LaLiga, Premier, Champions y Más</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Styles -->
-        @livewireStyles
-    </head>
-    <body >
-        <div class="font-sans  text-gray-900 antialiased">
-            {{ $slot }}
-        </div>
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-        @livewireScripts
-    </body>
+    <!-- Styles -->
+    @livewireStyles
+</head>
+
+<body>
+    <h1 style="display: none;">Estadísticas de Fútbol para apuestas deportivas</h1>
+    <section style="display: none;">
+        <p>Las mejores estadísticas de fútbol de LaLiga, Premier League, Bundesliga, Primeira Liga, Serie A, Champions League, Copa Libertadores y más.</p>
+    </section>
+    <div class="font-sans  text-gray-900 antialiased">
+        {{ $slot }}
+    </div>
+
+    @livewireScripts
+</body>
+
 </html>
